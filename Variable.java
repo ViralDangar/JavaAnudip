2fbedfaf574a11ae2afabf9fe21d4100e7b93d52
ViralDangar <98204435+ViralDangar@users.noreply.github.com>
2022-09-08 21:05:06 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,31 @@
+// Name=Viral Raja Dangar
+// batch=C2533
+
+public class Variable {
+    public static void main(String args[])
+        {
+            int in;//Instance Variable Inside Class outside Method
+            int c=5;//Instance Variable
+            System.out.println("Instance Variable=" +c);
+
+            public void case()
+            {
+                int b=10; //local variable
+                System.out.println("Value of b : "+b);
+            }
+
+            static int b=50;//Static Variable Inside Class outside Method with static keyword
+
+            public static void main(String[] args){
+            Localv l1 = new Localv();//Creating obj for Instance
+
+            System.out.println("Instance Variable i : "+l1.i);//default value of x will be printed
+            System.out.println("Instance Variable j : "+l1.j);
+
+            Localv l2 = new Localv();//Creating object for Local
+            l2.case();
+            System.out.println("Static Variable b : "+l2.b);
+
+        }
+        }
+}
